dda66d40baa4eba2b46b89ef9587d39b10ea7225
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-12-31 16:52:53 +1100

    POST request check added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import {
   BatchExecuteStatementCommand,
   BatchStatementRequest,
 } from '@aws-sdk/client-dynamodb'
-import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyHandlerV2,
+  APIGatewayProxyResultV2,
+} from 'aws-lambda'
 import { logger } from './utils/logger/buildLogger'
 import { hydrateEnv } from './utils/secrets/hydrateEnv'
-import { logAndThrowError } from './utils/logger/loggerHelpers'
+import {
+  logAndReturnError,
+  logAndThrowError,
+} from './utils/logger/loggerHelpers'
 import { validateSingleProductInBody } from './utils/validators/productDataValidators'
 import { ddbDocClient } from './libs/ddbDocClient'
 import { DynamoDBResponses, StatusCodes } from './types/dataTypes'
@@ -16,10 +23,14 @@ process.on('uncaughtException', (err) => {
   process.exit(1)
 })
 
-export const handler: APIGatewayProxyHandler = async (
-  event
-): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2,
+  context
+): Promise<APIGatewayProxyResultV2> => {
   logger.info(`Handler function called with event: ${JSON.stringify(event)}`)
+  logger.info(
+    `Handler function called with context: ${JSON.stringify(context)}`
+  )
   await hydrateEnv()
 
   if (!event.body) {
@@ -27,6 +38,13 @@ export const handler: APIGatewayProxyHandler = async (
       name: 'No arguments provided',
       message:
         'Please provide an array of products with all the required properties',
+    })
+  }
+
+  if (event.requestContext.http.method !== 'POST') {
+    return logAndReturnError(`Please provide a valid http method`, {
+      name: 'Only POST is supported',
+      message: `Please send a POST http request to add a new product`,
     })
   }
 
@@ -52,22 +70,24 @@ export const handler: APIGatewayProxyHandler = async (
 
     const { id, slug, name, variations } = parsedBody[index]
     const currentDate = new Date().toISOString()
-    Statements.push({
-      Statement:
-        'INSERT INTO ' +
-        process.env.WORDPRESS_PRODUCTS_TABLE_NAME +
-        "  value  {'id':?, 'slug':?, 'variations':?, 'name':?, 'date_created_gmt':?, 'date_modified_gmt':?}",
-      Parameters: [
-        { S: id },
-        { S: slug },
-        { SS: variations },
-        { S: name },
-        {
-          S: currentDate,
-        },
-        { S: currentDate },
-      ],
-    })
+    if (event.requestContext.http.method === 'POST') {
+      Statements.push({
+        Statement:
+          'INSERT INTO ' +
+          process.env.WORDPRESS_PRODUCTS_TABLE_NAME +
+          "  value  {'id':?, 'slug':?, 'variations':?, 'name':?, 'date_created_gmt':?, 'date_modified_gmt':?}",
+        Parameters: [
+          { S: id },
+          { S: slug },
+          { SS: variations },
+          { S: name },
+          {
+            S: currentDate,
+          },
+          { S: currentDate },
+        ],
+      })
+    }
   }
 
   const params = {
@@ -90,6 +110,31 @@ export const handler: APIGatewayProxyHandler = async (
   }
 
   // TODO: Add error handling for the many different dynamo DB errors
+  //   {
+  //     "errorType": "ValidationException",
+  //     "errorMessage": "1 validation error detected: Value '[]' at 'statements' failed to satisfy constraint: Member must have length greater than or equal to 1",
+  //     "name": "ValidationException",
+  //     "$fault": "client",
+  //     "$metadata": {
+  //         "httpStatusCode": 400,
+  //         "requestId": "4FTE3TVOBKALDPF9S4406G2H97VV4KQNSO5AEMVJF66Q9ASUAAJG",
+  //         "attempts": 1,
+  //         "totalRetryDelay": 0
+  //     },
+  //     "__type": "com.amazon.coral.validate#ValidationException",
+  //     "message": "1 validation error detected: Value '[]' at 'statements' failed to satisfy constraint: Member must have length greater than or equal to 1",
+  //     "stack": [
+  //         "ValidationException: 1 validation error detected: Value '[]' at 'statements' failed to satisfy constraint: Member must have length greater than or equal to 1",
+  //         "    at t3 (/var/task/index.js:3:4637)",
+  //         "    at K4 (/var/task/index.js:3:84994)",
+  //         "    at processTicksAndRejections (node:internal/process/task_queues:96:5)",
+  //         "    at async /var/task/index.js:1:13271",
+  //         "    at async /var/task/index.js:14:4933",
+  //         "    at async /var/task/index.js:3:302295",
+  //         "    at async /var/task/index.js:3:284950",
+  //         "    at async Runtime.hIe [as handler] (/var/task/index.js:56:28115)"
+  //     ]
+  // }
 
   return {
     statusCode: StatusCodes.SUCCESS,
